26314ba0e2046799ed8355c24840b38e55a35123
Karolina Raczynska <209357@student.pwr.edu.pl>
2017-03-10 22:45:08 +0100

    Template

---
+++ TeamOrganizer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TeamOrganizer.MvcApplication" Language="C#" %>
---
+++ TeamOrganizer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TeamOrganizer.Startup))]
+namespace TeamOrganizer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
