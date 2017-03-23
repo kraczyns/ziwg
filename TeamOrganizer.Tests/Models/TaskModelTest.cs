a9ca8a75244df04ebd4f467fc6d81b3f9a59b297
Piotr Maks <210102@student.pwr.edu.pl>
2017-03-23 21:31:52 +0100

    obsluga statusu i ID dla zadan + UT

---
+++ TeamOrganizer.Tests/Models/TaskModelTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TeamOrganizer.Models;
+
+namespace TeamOrganizer.Tests.Models
+{
+    [TestClass]
+    public class TaskModelTest
+    {
+        Random rnd = new Random();
+        
+        [TestMethod]
+        public void TaskId()
+        {
+            int id = rnd.Next();
+            TaskModel zadanie = new TaskModel();
+            zadanie.SetId(id);
+            Assert.AreEqual(id, zadanie.GetId());
+        }
+
+        [TestMethod]
+        public void Status()
+        {
+            TaskModel zadanie = new TaskModel();
+            foreach (Statuses sta in Enum.GetValues(typeof(Statuses)))
+            {
+                string status = sta.ToString();
+                Assert.IsTrue(zadanie.SetStatus(status));
+                Assert.AreEqual(status, zadanie.GetStatus());
+            }
+        }
+    }
+}
--- TeamOrganizer/Models/TaskModel.cs
+++ TeamOrganizer/Models/TaskModel.cs
@@ -5,7 +5,43 @@ using System.Web;
 
 namespace TeamOrganizer.Models
 {
+    public enum Statuses
+    {
+        New,
+        ToDo,
+        InProgress,
+        Done,
+        Closed
+    };
+
     public class TaskModel
     {
+        private int taskId;
+        private Statuses status;
+        
+        
+        public int GetId()
+        {
+            return taskId;
+        }
+        public void SetId(int aTaskId)
+        {
+            taskId = aTaskId;
+        }
+
+        public string GetStatus()
+        {
+            return status.ToString();
+        }
+
+        public bool SetStatus(string aStatus)
+        {
+            return Enum.TryParse(aStatus, out status);
+        }
+
+        public TaskModel()
+        {
+            
+        }
     }
 }
